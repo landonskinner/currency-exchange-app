8325ce5d475b00e68811a5a9a93059979defdf31
Landon Skinner <landonjskinner@gmail.com>
2021-11-15 14:58:12 -0600

    added basic nav functionality

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 function Header() {
     return (
         <div>
-            
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/currencies">Currencies</NavLink>
+            <NavLink to="/favorites">Favorites</NavLink>
         </div>
     )
 }
