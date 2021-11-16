d933a9046ea9919cb7db0106ee770e156fb76bfb
Landon Skinner <landonjskinner@gmail.com>
2021-11-16 07:43:01 -0600

    rendered/styled currency cards

--- src/CurrencyCard.js
+++ src/CurrencyCard.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-function CurrencyCard() {
+function CurrencyCard({currency, indivCurrency}) {
     return (
-        <div>
-            
+        <div className="currency-card">
+            <p className="currency-name">{indivCurrency[0].toUpperCase()}</p>
+            <p className="exchange-rate">Exchange Rate:</p>
+            <p className="exchange-value">{indivCurrency[1].toPrecision(5)}</p>
         </div>
     )
 }
--- src/CurrencyContainer.js
+++ src/CurrencyContainer.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownItem from 'react-bootstrap/DropdownItem'
 import CurrencyCard from './CurrencyCard';
 import NotesForm from './NotesForm';
 
-function CurrencyContainer() {
+function CurrencyContainer({currency, currencyExchange}) {
+    
+
+
+    const conversions = Object.entries(currencyExchange[currency])
+    
+    const currencyRender = conversions.map(indivCurrency => {
+        return <CurrencyCard key={indivCurrency[0]} currency={currency} indivCurrency={indivCurrency} />
+    })
+    
     return (
         <div>
-            
+           {currencyRender}
         </div>
     )
 }
